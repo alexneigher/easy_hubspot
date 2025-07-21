c7d40a981f27d865aeac41b586abc4f2e129e5a3
Alex Neigher <aneigher@gmail.com>
2025-07-20 20:20:10 -0700

    adding company CRUD

---
+++ lib/easy_hubspot/company.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module EasyHubspot
+  # class EasyHubspot::Contact
+  class Company < EasyHubspot::Base
+    class << self
+      COMPANY_ENDPOINT = 'crm/v3/objects/companies'
+
+      def get_company(company_id, access_token = nil)
+        Client.do_get("#{COMPANY_ENDPOINT}/#{company_id}", headers(access_token))
+      end
+
+      def get_companies(access_token = nil)
+        Client.do_get(COMPANY_ENDPOINT, headers(access_token))
+      end
+
+      def create_company(body, access_token = nil)
+        Client.do_post(COMPANY_ENDPOINT, body, headers(access_token))
+      end
+
+      def update_company(company_id, body, access_token = nil)
+        Client.do_patch("#{COMPANY_ENDPOINT}/#{company_id}", body, headers(access_token))
+      end
+
+      def delete_company(company_id, access_token = nil)
+        Client.do_delete("#{COMPANY_ENDPOINT}/#{company_id}", headers(access_token))
+      end
+    end
+  end
+end
